895edb9b5804f1c5a797f4f4357eeea6e984f9bf
Petr Klimovitskiy <petr.klimovitskiy@gmail.com>
2018-03-07 12:09:06 +0300

    Added view of NavigationMenu

---
+++ app/src/main/java/com/roadassistance/roadassistanceapp/presentation/navigation/view/INavigationMenuView.java
@@ -0,0 +1,8 @@
+package com.roadassistance.roadassistanceapp.presentation.navigation.view;
+
+/**
+ * Created by 22 on 07.03.2018.
+ */
+
+public interface INavigationMenuView {
+}
---
+++ app/src/main/java/com/roadassistance/roadassistanceapp/presentation/navigation/view/NavigationMenuView.java
@@ -0,0 +1,14 @@
+package com.roadassistance.roadassistanceapp.presentation.navigation.view;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.roadassistance.roadassistanceapp.R;
+
+public class NavigationMenuView extends Fragment implements INavigationMenuView {
+
+}
