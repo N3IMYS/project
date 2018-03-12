daf1bafbb25ca5ad3850f63d29458d32bc6e428a
Petr Klimovitskiy <petr.klimovitskiy@gmail.com>
2018-03-12 12:03:51 +0300

    Refactored package navigation -> navigationmenu

--- app/src/main/java/com/roadassistance/roadassistanceapp/presentation/navigationmenu/view/INavigationMenuView.java
+++ app/src/main/java/com/roadassistance/roadassistanceapp/presentation/navigationmenu/view/INavigationMenuView.java
@@ -1,4 +1,4 @@
-package com.roadassistance.roadassistanceapp.presentation.navigation.view;
+package com.roadassistance.roadassistanceapp.presentation.navigationmenu.view;
 
 /**
  * Created by 22 on 07.03.2018.
---
+++ app/src/main/java/com/roadassistance/roadassistanceapp/presentation/navigationmenu/view/NavigationMenuView.java
@@ -0,0 +1,8 @@
+package com.roadassistance.roadassistanceapp.presentation.navigationmenu.view;
+
+
+import android.support.v4.app.Fragment;
+
+public class NavigationMenuView extends Fragment implements INavigationMenuView {
+
+}
